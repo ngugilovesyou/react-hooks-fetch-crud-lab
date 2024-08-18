95ffec82d3b68022b91710098d50616158929806
samuelgitau <amsamgitau@gmail.com>
2024-08-18 18:26:03 +0300

    complete lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
-
+import React ,{useEffect,useState} from "react";
+import QuestionItem from "./QuestionItem";
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((r)=>r.json())
+    .then(questionsData =>setQuestions(questionsData))
+  }, [])
+  
+  function handleDelete(id){
+    const updatedQuestions = questions.filter((question) => question.id !== id);
+    setQuestions(updatedQuestions);
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} handleDelete={handleDelete} />
+        ))}
+      </ul>
     </section>
   );
 }
